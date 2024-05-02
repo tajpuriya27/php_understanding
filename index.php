5154f72fe3dd36b1af4e07d598d968a1a0d58d6a
tajpuriya27 <tajpuriyasunil27@gmail.com>
2024-05-02 09:37:22 +0545

    feat: what php file can contain

--- index.php
+++ index.php
@@ -1,8 +1,42 @@
 <!DOCTYPE html>
 <html>
 
-<body>
+<head>
+    <title>PHP Syntax</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+            padding: 20px;
+            background-color: #f4f4f4;
+            text-align: center;
+        }
 
+        h1 {
+            color: #333;
+        }
+
+        #activate-js {
+            color: #333;
+            text-align: center;
+            margin-top: 20px;
+        }
+
+        button {
+            padding: 10px 20px;
+            background-color: #333;
+            color: #fff;
+            border: none;
+            cursor: pointer;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>This is HTML h1 tag</h1>
+    Simple text in php file
+    <br />
     <?php
     // This is a single-line comment
     # This is also a single-line comment
@@ -13,7 +47,13 @@
     */
     echo "Hello World using PHP!";
     ?>
-
+    <div id="activate-js"></div>
+    <button onclick="activateJS()">Activate JS</button>
+    <script>
+        function activateJS() {
+            document.getElementById("activate-js").innerHTML = "JavaScript activated!";
+        }
+    </script>
 </body>
 
 </html>
