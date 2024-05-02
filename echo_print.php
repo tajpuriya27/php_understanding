814fa85270c40ffb3fc7339e7c86ceae97fc1f96
tajpuriya27 <tajpuriyasunil27@gmail.com>
2024-05-02 14:21:53 +0545

    feat: echo and print statement file

---
+++ echo_print.php
@@ -0,0 +1,9 @@
+<?php
+echo ("Echo don't return any value, it just print the value on the screen.");
+echo "<br>";
+
+$return_print = print("Print also don't return any value, it just print the value on the screen.");
+echo "<br>";
+print("Print returns " . $return_print);
+
+echo ("<br>" . "printing print");
