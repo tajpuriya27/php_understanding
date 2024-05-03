bcea6c798f24669ea6e583300ad766d688f60d5a
tajpuriya27 <tajpuriyasunil27@gmail.com>
2024-05-03 08:44:28 +0545

    feat: PHP String

--- string.php
+++ string.php
@@ -1,4 +1,10 @@
+
+
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $var1 = "Hello I am Learning PHP";
 
 echo strlen($var1); // 23
@@ -19,3 +25,27 @@ $strVar = "Hello I am Learning PHP in lower case";
 echo strtoupper($strVar); // HELLO I AM LEARNING PHP IN LOWER CASE
 echo "<br>";
 echo "$strVar"; // Hello I am Learning PHP in lower case
+
+echo "<hr>";
+$strVar = "HELLO I AM LEARNING PHP IN Upper CASE";
+echo strtolower($strVar); // hello i am learning php in upper case
+echo "<br>";
+echo "$strVar"; // HELLO I AM LEARNING PHP IN LOWER CASE
+
+echo "<hr>";
+$phpvar = "Hello World!";
+echo str_replace("World", "Team", $phpvar);
+echo "<br>";
+echo "$phpvar";
+
+echo "<hr>";
+echo "<h3>Reverse a string</h3>";
+$x = "Hello World!";
+echo "<br/> After Reversing: " . strrev($x);
+echo "<br/> Before reversing: " . $x;
+
+echo "<hr>";
+$x = " Hello World! ";
+echo strlen(trim($x)); //12
+echo "<br>";
+echo strlen($x); // 14
